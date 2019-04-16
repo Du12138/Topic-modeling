aad53b850858c3557fcaba32c1adff866a1b89e5
Du12138 <47547890+Du12138@users.noreply.github.com>
2019-04-15 21:04:29 -0400

    Update Untitled1[1].py

--- Untitled1[1].py
+++ Untitled1[1].py
@@ -1,284 +1,79 @@
-<!DOCTYPE HTML>
-<html>
+import pandas as pd
+import gensim
+from gensim import corpora, models
+from gensim.utils import simple_preprocess
+from gensim.parsing.preprocessing import STOPWORDS
+from nltk.stem import WordNetLemmatizer, SnowballStemmer
+from nltk.stem.porter import *
+from pprint import pprint
+import numpy as np
+import nltk
+nltk.download('wordnet')
 
-<head>
-    <meta charset="utf-8">
+data = pd.read_csv('/Users/fdu12/OneDrive/Desktop/df.csv', error_bad_lines=False);
+data_text = data[['clean_content']]
+data_text['index'] = data_text.index
+documents = data_text
+print(len(documents))
+print(documents[:5])
 
-    <title>Jupyter Notebook</title>
-    <link id="favicon" rel="shortcut icon" type="image/x-icon" href="/static/base/images/favicon.ico?v=97c6417ed01bdc0ae3ef32ae4894fd03">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-    <link rel="stylesheet" href="/static/components/jquery-ui/themes/smoothness/jquery-ui.min.css?v=3c2a865c832a1322285c55c6ed99abb2" type="text/css" />
-    <link rel="stylesheet" href="/static/components/jquery-typeahead/dist/jquery.typeahead.min.css?v=7afb461de36accb1aa133a1710f5bc56" type="text/css" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+stemmer = SnowballStemmer('english')
+def lemmatize_stemming(text):
+    return stemmer.stem(WordNetLemmatizer().lemmatize(text, pos='v'))
     
-    
+def preprocess(text):
+    result = []
+    for token in gensim.utils.simple_preprocess(text):
+        if token not in gensim.parsing.preprocessing.STOPWORDS and len(token) > 3:
+            result.append(lemmatize_stemming(token))
+    return result
 
-    <link rel="stylesheet" href="/static/style/style.min.css?v=e79dbfdd7c8f19c96a1c8d0d7da38bdc" type="text/css"/>
-    
-<link rel="stylesheet" href="/static/auth/css/override.css?v=19ec59d2c4f1203c49fe47462028cd9a" type="text/css" />
-
-    <link rel="stylesheet" href="/custom/custom.css" type="text/css" />
-    <script src="/static/components/es6-promise/promise.min.js?v=f004a16cb856e0ff11781d01ec5ca8fe" type="text/javascript" charset="utf-8"></script>
-    <script src="/static/components/preact/index.js?v=00a2fac73c670ce39ac53d26640eb542" type="text/javascript"></script>
-    <script src="/static/components/proptypes/index.js?v=c40890eb04df9811fcc4d47e53a29604" type="text/javascript"></script>
-    <script src="/static/components/preact-compat/index.js?v=f865e990e65ad27e3a2601d8adb48db1" type="text/javascript"></script>
-    <script src="/static/components/requirejs/require.js?v=951f856e81496aaeec2e71a1c2c0d51f" type="text/javascript" charset="utf-8"></script>
-    <script>
-      require.config({
-          
-          urlArgs: "v=20190415192926",
-          
-          baseUrl: '/static/',
-          paths: {
-            'auth/js/main': 'auth/js/main.min',
-            custom : '/custom',
-            nbextensions : '/nbextensions',
-            kernelspecs : '/kernelspecs',
-            underscore : 'components/underscore/underscore-min',
-            backbone : 'components/backbone/backbone-min',
-            jed: 'components/jed/jed',
-            jquery: 'components/jquery/jquery.min',
-            json: 'components/requirejs-plugins/src/json',
-            text: 'components/requirejs-text/text',
-            bootstrap: 'components/bootstrap/dist/js/bootstrap.min',
-            bootstraptour: 'components/bootstrap-tour/build/js/bootstrap-tour.min',
-            'jquery-ui': 'components/jquery-ui/jquery-ui.min',
-            moment: 'components/moment/min/moment-with-locales',
-            codemirror: 'components/codemirror',
-            termjs: 'components/xterm.js/xterm',
-            typeahead: 'components/jquery-typeahead/dist/jquery.typeahead.min',
-          },
-          map: { // for backward compatibility
-              "*": {
-                  "jqueryui": "jquery-ui",
-              }
-          },
-          shim: {
-            typeahead: {
-              deps: ["jquery"],
-              exports: "typeahead"
-            },
-            underscore: {
-              exports: '_'
-            },
-            backbone: {
-              deps: ["underscore", "jquery"],
-              exports: "Backbone"
-            },
-            bootstrap: {
-              deps: ["jquery"],
-              exports: "bootstrap"
-            },
-            bootstraptour: {
-              deps: ["bootstrap"],
-              exports: "Tour"
-            },
-            "jquery-ui": {
-              deps: ["jquery"],
-              exports: "$"
-            }
-          },
-          waitSeconds: 30,
-      });
-
-      require.config({
-          map: {
-              '*':{
-                'contents': 'services/contents',
-              }
-          }
-      });
-
-      // error-catching custom.js shim.
-      define("custom", function (require, exports, module) {
-          try {
-              var custom = require('custom/custom');
-              console.debug('loaded custom.js');
-              return custom;
-          } catch (e) {
-              console.error("error loading custom.js", e);
-              return {};
-          }
-      })
-
-    document.nbjs_translations = {"domain": "nbjs", "locale_data": {"nbjs": {"": {"domain": "nbjs"}}}};
-    document.documentElement.lang = navigator.language.toLowerCase();
-    </script>
-
-    
-    
-
-</head>
-
-<body class=""
+doc_sample = documents[documents['index'] == 100].values[0][0]
  
-  
+print('clean_content')
+words = []
+for word in doc_sample.split(' '):
+    words.append(word)
+print(words)
+print('\n\n tokenized and lemmatized document: ')
+print(preprocess(doc_sample))
+
+processed_docs = documents['clean_content'].map(preprocess)
+processed_docs[:10]
+
+dictionary = gensim.corpora.Dictionary(processed_docs)
+for i in range(10):
+    print(i,dictionary[i])
+print('number of total words:',len(dictionary))
+
+for i in range(10):
+    print(i,dictionary.dfs[i])
+    
+dictionary.filter_extremes(no_below=15, no_above=0.5, keep_n=100000)
+len(dictionary)
+print('number of total words:',len(dictionary))
+
+bow_corpus = [dictionary.doc2bow(doc) for doc in processed_docs]
+print(processed_docs[0])
+print(bow_corpus[0])
+
+bow_doc_0 = bow_corpus[0]
+for i in range(len(bow_doc_0)):
+    print("Word {} (\"{}\") appears {} time.".format(bow_doc_0[i][0], 
+                                               dictionary[bow_doc_0[i][0]], 
+bow_doc_0[i][1]))
+
+tfidf = models.TfidfModel(bow_corpus)
+corpus_tfidf = tfidf[bow_corpus]
+for doc in corpus_tfidf:
+    pprint(doc)
+    break
+    
+lda_model = gensim.models.LdaMulticore(bow_corpus, num_topics=10, id2word=dictionary, passes=2, workers=2)
+for idx, topic in lda_model.print_topics(-1):
+    print('Topic: {} \nWords: {}'.format(idx, topic))
+
+lda_model_tfidf = gensim.models.LdaMulticore(corpus_tfidf, num_topics=1000, id2word=dictionary, passes=2, workers=4)
  
-dir="ltr">
-
-<noscript>
-    <div id='noscript'>
-      Jupyter Notebook requires JavaScript.<br>
-      Please enable it to proceed. 
-  </div>
-</noscript>
-
-<div id="header">
-  <div id="header-container" class="container">
-  <div id="ipython_notebook" class="nav navbar-brand"><a href="/tree" title='dashboard'>
-      <img src='/static/base/images/logo.png?v=641991992878ee24c6f3826e81054a0f' alt='Jupyter Notebook'/>
-  </a></div>
-
-  
-  
-  
-  
-  
-  
-
-
-  
-  
-  </div>
-  <div class="header-bar"></div>
-
-  
-  
-</div>
-
-<div id="site">
-
-
-<div id="ipython-main-app" class="container">
-
-    
-    
-    <div class="row">
-    <div class="navbar col-sm-8">
-      <div class="navbar-inner">
-        <div class="container">
-          <div class="center-nav">
-            <form action="/login?next=%2Fedit%2FUntitled1.py" method="post" class="navbar-form pull-left">
-              <input type="hidden" name="_xsrf" value="2|537e3bf7|3ceb5fb32b462bfe320ec032c788be27|1555376570"/>
-              
-                <label for="password_input"><strong>Password or token:</strong></label>
-              
-              <input type="password" name="password" id="password_input" class="form-control">
-              <button type="submit" class="btn btn-default" id="login_submit">Log in</button>
-            </form>
-          </div>
-        </div>
-      </div>
-    </div>
-    </div>
-    
-    
-    
-    
-    <div class="col-sm-6 col-sm-offset-3 text-left rendered_html">
-      <h3>
-        Token authentication is enabled
-      </h3>
-      <p>
-        If no password has been configured, you need to open the notebook
-        server with its login token in the URL, or paste it above.
-        This requirement will be lifted if you
-        <b><a href='https://jupyter-notebook.readthedocs.io/en/stable/public_server.html'>
-            enable a password</a></b>.
-      </p>
-      <p>
-        The command:
-        <pre>jupyter notebook list</pre>
-        will show you the URLs of running servers with their tokens,
-        which you can copy and paste into your browser. For example:
-      </p>
-      <pre>Currently running servers:
-http://localhost:8888/?token=c8de56fa... :: /Users/you/notebooks
-</pre>
-      <p>
-        or you can paste just the token value into the password field on this
-        page.
-      </p>
-      <p>
-        See
-        <b><a
-         href='https://jupyter-notebook.readthedocs.io/en/stable/public_server.html'>
-                the documentation on how to enable a password</a>
-        </b>
-        in place of token authentication,
-        if you would like to avoid dealing with random tokens.
-      </p>
-      <p>
-        Cookies are required for authenticated access to notebooks.
-      </p>
-      
-        <h3>Setup a Password</h3>
-        <p> You can also setup a password by entering your token and a new password
-        on the fields below:</p>
-        <form action="/login?next=%2Fedit%2FUntitled1.py" method="post" class="">
-                <input type="hidden" name="_xsrf" value="2|537e3bf7|3ceb5fb32b462bfe320ec032c788be27|1555376570"/>
-          <div class="form-group">
-            <label for="token_input"><h4>Token</h4></label>
-            <input type="password" name="password" id="token_input" class="form-control">
-          </div>
-          <div class="form-group">
-            <label for="new_password_input"><h4>New Password</h4></label>
-            <input type="password" name="new_password" id="new_password_input" class="form-control" required>
-          </div>
-          <div class="form-group">
-            <button type="submit" class="btn btn-default" id="login_new_pass_submit">Log in and set new password</button>
-          </div>
-        </form>
-      
-
-    </div>
-    
-    
-</div>
-
-
-</div>
-
-
-
-
-
-
-
-
-<script type="text/javascript">
-  require(["auth/js/main"], function (auth) {
-    auth.login_main();
-  });
-</script>
-
-
-
-<script type='text/javascript'>
-  function _remove_token_from_url() {
-    if (window.location.search.length <= 1) {
-      return;
-    }
-    var search_parameters = window.location.search.slice(1).split('&');
-    for (var i = 0; i < search_parameters.length; i++) {
-      if (search_parameters[i].split('=')[0] === 'token') {
-        // remote token from search parameters
-        search_parameters.splice(i, 1);
-        var new_search = '';
-        if (search_parameters.length) {
-          new_search = '?' + search_parameters.join('&');
-        }
-        var new_url = window.location.origin + 
-                      window.location.pathname + 
-                      new_search + 
-                      window.location.hash;
-        window.history.replaceState({}, "", new_url);
-        return;
-      }
-    }
-  }
-  _remove_token_from_url();
-</script>
-</body>
-
-</html>
+for idx, topic in lda_model_tfidf.print_topics(-1):
+    print('Topic: {} Word: {}'.format(idx, topic))
